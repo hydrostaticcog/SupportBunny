03e1480aac573afe4780166ddcc55dff6abe1f5d
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-23 18:40:10 -0600

    update/fix ping

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,11 @@ module.exports ={
     name: "ping",
     description: "responds with bot latency",
     args: false,
-    execute(message, args){
+    execute(client, message, args){
         message.channel.send('Calculating ping...').then((resultMessage) => {
-        const ping = resultMessage.createdTimestamp - message.createdTimestamp
+            const ping = resultMessage.createdTimestamp - message.createdTimestamp
   
-        resultMessage.edit(`:ping_pong: Pong! **Bot Latency:** ${ping}ms, **API Latency:** ${client.ws.ping}ms`)
+            resultMessage.edit(`:ping_pong: Pong! **Bot Latency:** ${ping}ms, **API Latency:** ${client.ws.ping}ms`)})
+
     }
 }
